b6e3e5b4827cbd813cdc5ed2adb654df84a86da8
Jewell Shanin Galdo <galdo.jewellshanin16@gmail.com>
2023-04-22 10:36:00 +0800

    updated

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             color: #636b6f;
             font-family: 'Nunito', sans-serif;
             font-weight: 200;
-            height: 50vh;
+            height: 100vh;
             margin: 0;
         }
 
